9cabbbe035d451207f8bcc48a6478ce559265061
sogawa-yk <daybreaks.link@gmail.com>
2020-05-07 18:20:32 +0900

    exam

--- 5/cat.c
+++ 5/cat.c
@@ -4,3 +4,16 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+
+static void do_cat(const char *path);
+static void die(const char *s);
+
+int
+main(int argc, char *argv[])
+{
+    int i;
+
+    if(argc < 2){
+        fprintf(stderr, "%s: file name not given\n", argv[0])
+    }
+}
